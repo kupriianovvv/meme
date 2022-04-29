10cb349a5d346d9854a28e61839fd91de26475d8
kupriianovvv <kupriianovvv@gmail.com>
2022-04-29 12:46:21 +0300

    last

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import trollface from "../images/trollface.png"
+
+import '../style.css'
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src={trollface}
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
